5ace106c8b5783eb1deeb45d09c6e5c270a85f7b
LukeLinXu <dev.linxu@gmail.com>
2016-03-16 18:24:18 -0400

    Update JsonResponseBodyConverter.java

--- src/main/java/retrofit/JsonResponseBodyConverter.java
+++ src/main/java/retrofit/JsonResponseBodyConverter.java
@@ -1,10 +1,12 @@
 package retrofit;
 
-import com.squareup.okhttp.ResponseBody;
+import java.io.IOException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import retrofit.Converter;
-import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
 
 final class JsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
 
@@ -18,8 +20,9 @@ final class JsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
         try {
             jsonObj = new JSONObject(value.string());
             return (T) jsonObj;
-        } catch(JSONException e) {
+        } catch (JSONException e) {
             return null;
         }
     }
+
 }
